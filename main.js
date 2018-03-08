2f9a91cde41b1f2ad217fbdffcf9e7622c4b3d7c
Filipe Esperandio <filipesperandio@gmail.com>
2018-03-08 10:40:05 -0300

    Use native net component

--- main.js
+++ main.js
@@ -1,28 +1,33 @@
-const {app, Tray} = require("electron");
-const fetch = require("fetch");
+const {app, Tray, net} = require("electron");
+
+const CHECK_INTERVAL_MIN = 10;
 
 let tray = null;
 
 app.on("ready", () => {
-  const good = "./green24.png";
-  const bad = "./red24.png";
+  const success = "./green24.png";
+  const failure = "./red24.png";
   const loading = "./loading24.png";
   const CHECK_INTERVAL_MIN = 10;
 
   tray = new Tray(loading);
 
-  function changeImage(img) {
+  const changeImage = (img) => {
     tray.setImage(img);
-  }
+  };
 
-  function hazInternetz() {
-    console.log("checking");
+  const onFailure = () => changeImage(failure);
+
+  const onSuccess = () => changeImage(success);
+
+  const hazInternetz = () => {
     changeImage(loading);
 
-    fetch.fetchUrl("http://www.google.com", (error) => {
-      console.log("haz internetz", !error);
-      changeImage(!error ? good : bad);
-    });
+    const request = net.request("http://www.google.com");
+    request.on("response", onSuccess);
+    request.on("error", onFailure);
+
+    request.end();
   }
 
   tray.on("click", hazInternetz);
